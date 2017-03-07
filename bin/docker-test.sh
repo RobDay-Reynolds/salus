6830c1edee9a4c3b812a135391941fd6c00fb12c
DennisDenuto <dennis.leon39@gmail.com>
2017-03-06 20:58:24 -0800

    Add info regarding failing icmp test when running test in docker for mac

--- bin/docker-test.sh
+++ bin/docker-test.sh
@@ -4,6 +4,10 @@ bin=$(dirname $0)
 
 cd $(dirname $bin)
 
+echo '##############################'
+echo 'FYI: ICMP test will fail due to docker for mac issue (https://github.com/docker/for-mac/issues/57)'
+echo '##############################'
+
 go get github.com/onsi/ginkgo/ginkgo
 sysctl -w net.ipv4.ping_group_range="0 65535"
 
