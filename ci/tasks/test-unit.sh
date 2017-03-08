ee752318fe7dcf9628b65b6043b78e4c7625f50c
Rob Day-Reynolds <rdayreynolds@pivotal.io>
2017-03-07 17:19:54 -0800

    Add scaffolding for Concourse pipeline

---
+++ ci/tasks/test-unit.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e -x
+
+source ~/.bashrc
+
+export PATH=/usr/local/ruby/bin:/usr/local/go/bin:$PATH
+export GOPATH=$(pwd)/gopath
+
+cd gopath/src/github.com/monkeyherder/moirai
+bin/require-ci-golang-version
+bin/clean
+bin/install-ginkgo
+bin/test-prepare
+bin/test-unit
+
